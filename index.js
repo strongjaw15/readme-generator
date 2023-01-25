0c048706a07c20dba6de2c6fe83e0b14bc4560c5
strongjaw15 <119770691+strongjaw15@users.noreply.github.com>
2023-01-24 21:26:07 -0600

    Debugged the write file functionality, installed nodemon.

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ inquirer
       - What was your motivation?
       - Why did you build this project?
       - What problem does it solve?
-      - What did you learn?`,
+      - What did you learn?
+      `,
       name: "description"
     },
     {
@@ -108,11 +109,11 @@ inquirer
     
     ${data.credits}`;
 
-    // fs.writeFile("index.html", finalString, (err) => {
-    //   if (err)
-    //     console.log(err);
-    // })
-    fs.writeFile("index.html", finalString, null)
-
-    // fs.writeFile("index.html", finalString)
+    fs.writeFile("index.html", finalString, (err) => {
+      if (err){
+        console.log(err)
+      } else {
+        console.log("README successfully written!")
+      }
+    })
   })
